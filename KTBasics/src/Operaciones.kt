210b84c868d48484507460f65d70d2615c671eec
LazarusPolar <roberto.almaraz@habilgroup.net>
2018-04-09 01:02:24 -0500

    Agrega especificacion de operaciones basicas

---
+++ KTBasics/src/Operaciones.kt
@@ -0,0 +1,13 @@
+fun main(args: Array<String>){
+
+    println("Escribe el primer valor: ")
+    var x: Float = readLine()!!.toFloat()
+    println("Escribe el primer valor: ")
+    var y: Float = readLine()!!.toFloat()
+    var suma: Float?
+
+    suma = x + y
+
+    print("Operacion de suma: $suma")
+
+}
