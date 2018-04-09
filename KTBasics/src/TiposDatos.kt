7c84074fd20b61eb960428c0094dbf2abebd20ea
LazarusPolar <roberto.almaraz@habilgroup.net>
2018-04-08 23:37:23 -0500

    Agrega archivo TiposDatos

---
+++ KTBasics/src/TiposDatos.kt
@@ -0,0 +1,15 @@
+fun main(args: Array<String>){
+
+    // Enteros
+    var x = 10
+    var y: Int = 20
+
+    // Cadenas
+    var nombre: String = "Cadena"
+    var asignacion: String? // Acepta que el valor sea vacio
+
+    asignacion = "Hola"
+
+    println("Asignacion: $asignacion")
+
+}
