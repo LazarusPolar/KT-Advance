11d4ecc8af8fca2fcf2f45f740a8ffefefb72cc1
LazarusPolar <roberto.almaraz@habilgroup.net>
2018-04-09 00:00:00 -0500

    Agrega ejemplo de Null Safety en Kotlin

---
+++ KTBasics/src/NullSafety.kt
@@ -0,0 +1,7 @@
+fun main(args: Array<String>){
+
+    //var nombre: String? // Acepta valores nulos
+    var nombre: String? // Acepta valores nulos
+    nombre = "Jose A."
+    println("Nombre: ${nombre!!}") // "!!" Obliga a que el valor no debe ser null al momento de ejecutar la linea de codigo
+}
