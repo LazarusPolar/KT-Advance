2034c803296e3267df38429e71a141f35ff9ef46
LazarusPolar <roberto.almaraz@habilgroup.net>
2018-04-11 00:01:39 -0500

    Agrega ejemplo de for anidado

---
+++ KTBasics/src/ForAnidado.kt
@@ -0,0 +1,10 @@
+fun main(args: Array<String>) {
+
+
+    for(item in 2..10){
+        println("Hola mundo ${item}")
+        for(item2 in 1..5){
+            println("Coordenadas ${item}, ${item2}")
+        }
+    }
+}
