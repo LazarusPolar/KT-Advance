5a365ab99c52e202fddbfb81fd0a5165c87c7bc9
LazarusPolar <roberto.almaraz@habilgroup.net>
2018-04-14 18:45:06 -0500

    Agrega ejemplo de variables locales y globales

---
+++ KTBasics/src/LocalGlobalVarsScope.kt
@@ -0,0 +1,13 @@
+// Variable Global
+var nombreCompleto = "Jose Almaraz"
+
+fun muestra(): Unit {
+    println("Nombre Complet: $nombreCompleto")
+}
+
+fun main(args: Array<String>) {
+    // Variables Locales
+    var nombre = "Jose"
+    println("Nombre $nombre")
+    println("Nombre Complet: $nombreCompleto")
+}
