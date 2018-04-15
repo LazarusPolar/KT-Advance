d33b441de049a264898d8b1bf6730f3de1eebb00
LazarusPolar <roberto.almaraz@habilgroup.net>
2018-04-15 02:09:17 -0500

    Agrega ejemplo de herencia

---
+++ KTBasics/src/Herencia.kt
@@ -0,0 +1,39 @@
+open class Operaciones() {
+    fun suma(n1: Int, n2: Int): Int{
+        return n1+n2
+    }
+
+    fun dividir(n1:Int, n2:Int): Int {
+        return n1/n2
+    }
+}
+
+// Herada las funciones de la clase Operaciones
+// Nota: siempre colocar open a la clase a la que se hereda ya que, sin ello, no se permitira dicha herencia
+class Mutltioperaciones():Operaciones() {
+
+    fun resta(n1: Int, n2: Int): Int{
+        return n1-n2
+    }
+
+    fun multiplicacion(n1:Int, n2:Int): Int {
+        return n1*n2
+    }
+}
+
+fun main(args: Array<String>) {
+    var op = Operaciones()
+    var suma = op.suma(1,2)
+    println("Imprime Suma: $suma")
+
+    var dividir = op.dividir(1, 2)
+    println("Imprime Division $dividir")
+
+    var op2 = Mutltioperaciones()
+    var suma1 = op2.suma(1,2)
+    println("Imprime Suma: $suma1")
+
+    var dividi1r = op2.dividir(1, 2)
+    println("Imprime Division $dividi1r")
+
+}
