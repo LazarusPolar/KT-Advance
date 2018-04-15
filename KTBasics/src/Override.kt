d646e650b28ec889825dd4f288330998b6aee2e5
LazarusPolar <roberto.almaraz@habilgroup.net>
2018-04-15 02:16:58 -0500

    Se agrega ejemplo de override

---
+++ KTBasics/src/Override.kt
@@ -0,0 +1,54 @@
+open class Operaciones1() {
+    public var nombreProceso: String? = null
+    // protected - Pueden verla en la clase y los que heredan
+    // private - Solo puede verlo la clase de origen
+
+    // Para realizar un override, la funcion debe tener el modificador open
+    open fun suma(n1: Int, n2: Int): Int{
+        return n1+n2
+    }
+
+    fun dividir(n1:Int, n2:Int): Int {
+        return n1/n2
+    }
+}
+
+// Herada las funciones de la clase Operaciones
+// Nota: siempre colocar open a la clase a la que se hereda ya que, sin ello, no se permitira dicha herencia
+class Mutltioperaciones1():Operaciones1() {
+
+    // Se sobrecarga la funcion y puede modificarse su valor de retorno
+    override fun suma(n1: Int, n2: Int): Int{
+        return n1+n2*3
+    }
+
+    fun resta(n1: Int, n2: Int): Int{
+        return n1-n2
+    }
+
+    fun multiplicacion(n1:Int, n2:Int): Int {
+        return n1*n2
+    }
+
+    fun obtieneNombre() {
+        super.nombreProceso
+    }
+}
+
+fun main(args: Array<String>) {
+    var op = Operaciones1()
+    var suma = op.suma(1,2)
+    println("Imprime Suma: $suma")
+
+    var dividir = op.dividir(1, 2)
+    println("Imprime Division $dividir")
+
+    var op2 = Mutltioperaciones1()
+    var suma1 = op2.suma(1,2)
+    println("Imprime Suma: $suma1")
+
+    var dividi1r = op2.dividir(1, 2)
+    println("Imprime Division $dividi1r")
+
+    println("op.nombreProceso: ${op.nombreProceso}")
+}
